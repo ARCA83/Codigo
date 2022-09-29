449f36ea266ff4d51c4934f75db4b05e182c058f
ARCA83 <anthonycruz0709@gmail.com>
2022-09-29 18:30:07 -0500

    "firstjava"

---
+++ javascript/semana5/operaciones.js
@@ -0,0 +1,18 @@
+// string numbre = tipos
+let n1=10;
+let n2=20;
+
+let verdadero = true; // booleam
+let falso = false; // booleam
+
+let nombre = "Linder" // string
+
+let direccion = null; // null existe pero su valor no fue encontrado
+
+let suma = n1 + n2; //30
+
+let resta = n1 - n2;//-10
+
+let producto = n1 * n2; //200
+
+let division = n1 /  n2; //0.5
