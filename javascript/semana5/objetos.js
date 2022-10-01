894647474a95f8e26613a21680b97c6d06d20b0b
ARCA83 <anthonycruz0709@gmail.com>
2022-09-30 20:44:59 -0500

    final

--- javascript/semana5/objetos.js
+++ javascript/semana5/objetos.js
@@ -1,8 +1,43 @@
-let persona;
 
+
+const personas=[];
 
 function crearPersona() {
-     persona ={
+     const persona1 ={
+        name:"bruno",
+        years: 32,
+        gender: "masculino",
+        talla:179,
+        is_adult: function(){
+            return this.year >=18;
+        },
+        inches:function(){
+            return this.talla /2.54;
+        },
+        upper_name: function (){
+            return this.name.toUpperCase();
+        },
+        captital_name: function (){
+            return (this.upper_name(),substring(0,1)+this.name.substring(1, this.name, lenght))
+        }
+        const persona1 ={
+            name:"bruno",
+            years: 32,
+            gender: "masculino",
+            talla:179,
+            is_adult: function(){
+                return this.year >=18;
+            },
+            inches:function(){
+                return this.talla /2.54;
+            },
+            upper_name: function (){
+                return this.name.toUpperCase();
+            },
+            captital_name: function (){
+                return (this.upper_name(),substring(0,1)+this.name.substring(1, this.name, lenght))
+            }
+            const persona ={
         name:"bruno",
         years: 32,
         gender: "masculino",
@@ -20,7 +55,9 @@ function crearPersona() {
             return (this.upper_name(),substring(0,1)+this.name.substring(1, this.name, lenght))
         }
 
+    
     };
+
     console.log(persona);
 
 }
